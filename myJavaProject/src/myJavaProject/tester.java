12e729a8be368a50bff58f972e89db4a0b62df8c
Sanchita <jsanchita4500@gmail.com>
2020-05-21 17:25:31 -0700

    changes in src comiited

--- myJavaProject/src/myJavaProject/tester.java
+++ myJavaProject/src/myJavaProject/tester.java
@@ -3,6 +3,8 @@
  */
 package myJavaProject;
 
+import java.util.Scanner;
+
 /**
  * @author jsanc
  *
@@ -14,7 +16,15 @@ public class tester {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Hello Software Engineering World");
+		System.out.println("Enter your name");
+		String name = input.nextLine();
+		System.out.println("Your name is :" + name);
+		System.out.println("Enter your age: ");
+		int age = input.nextInt();
+		System.out.println("Your age is: " + age);
 	}
+	
 
 }
