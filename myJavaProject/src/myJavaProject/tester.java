d6fee5202b08cf1792bf518f1b62e1a61b395b85
Sanchita <jsanchita4500@gmail.com>
2020-05-21 17:47:53 -0700

    variables added

--- myJavaProject/src/myJavaProject/tester.java
+++ myJavaProject/src/myJavaProject/tester.java
@@ -15,15 +15,20 @@ public class tester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		int a;
+		String name; 
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		System.out.println("Hello Software Engineering World");
 		System.out.println("Enter your name");
-		String name = input.nextLine();
+		name = input.nextLine();
 		System.out.println("Your name is :" + name);
 		System.out.println("Enter your age: ");
 		int age = input.nextInt();
 		System.out.println("Your age is: " + age);
+		for(a=0; a < name.length(); a++) {
+			System.out.println("*");
+		}
 	}
 	
 
